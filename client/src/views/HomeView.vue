e189967b39784e64066938685649d8a04a0f9761
nadi <nadavcn@gmail.com>
2026-09-17 19:13:00 +0200

    feat(client): add Home screen

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
 
 <template>
-  <div />
+  <div class="flex min-h-screen flex-col items-center justify-center gap-4">
+    <Button size="lg" @click="router.push({ name: 'random-list' })">Fetch</Button>
+    <Button size="lg" variant="secondary" @click="router.push({ name: 'saved-list' })">
+      History
+    </Button>
+  </div>
 </template>
